5144c49ca19f3f2599d6e606208d80c1bdd12d70
SunJin_Yoon <sunjin.yoon@fnsusa.com>
2025-02-10 19:35:48 -0800

    added Common Component

---
+++ src/components/posts/PostForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  title: String,
+  content: String,
+});
+
+defineEmits(['update:title', 'update:content']); // 'submit' 추가
+</script>
+
+<template>
+  <div>
+    <form>
+      <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input :value="title" @input="$emit('update:title', $event.target.value)" type="text" class="form-control" id="title" placeholder="Title">
+      </div>
+      <div class="mb-3">
+        <label for="content" class="form-label">Contents</label>
+        <textarea :value="content" @input="$emit('update:content', $event.target.value)" class="form-control" id="content" rows="3"></textarea>
+      </div>
+      <div class="d-flex gap-2 me-2">
+        <slot name="actions"></slot>
+      </div>
+    </form>
+  </div>
+</template>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -3,6 +3,7 @@
 import { useRoute, useRouter } from 'vue-router'
 import { ref } from 'vue'
 import { getPostById, updatePost } from '@/api/post.js'
+import PostForm from '@/components/posts/PostForm.vue'
 
 const route =useRoute();
 const router =useRouter();
@@ -46,20 +47,12 @@ const edit = async () => {
   <div>
     <h2>Edit Post</h2>
     <hr class="my-4"/>
-    <form @submit.prevent="edit">
-      <div class="mb-3">
-        <label for="title" class="form-label">Title</label>
-        <input v-model="form.title" type="text" class="form-control" id="title" placeholder="Title">
-      </div>
-      <div class="mb-3">
-        <label for="contents" class="form-label">Contents</label>
-        <textarea v-model="form.content" class="form-control" id="contents" rows="3"></textarea>
-      </div>
-      <div class="pt-4">
-        <button type="button" class="btn btn-outline-dark me-2" @click="goDetailPage">Cancel</button>
+    <PostForm v-model:title="form.title" v-model:content="form.content" @submit.prevent="edit">
+      <template #actions>
+        <button type="button" class="btn btn-outline-dark" @click="goDetailPage">Cancel</button>
         <button class="btn btn-primary">Edit</button>
-      </div>
-    </form>
+      </template>
+    </PostForm>
   </div>
 
 </template>
